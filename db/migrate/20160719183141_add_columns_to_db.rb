1b168de6686a413faad4108abcf212ca51772206
Rickard Dahlstrom <rickarddahlstrom@Rickards-MBP.lan>
2016-07-19 21:51:30 +0200

    added article table and model

---
+++ db/migrate/20160719183141_add_columns_to_db.rb
@@ -0,0 +1,4 @@
+class AddColumnsToDb < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ db/migrate/20160719184247_add_articles_table.rb
@@ -0,0 +1,11 @@
+class AddArticlesTable < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
+
+
