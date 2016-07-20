8085342bdc722fb904d61e61cf929543dce501f9
Rickard Dahlstrom <rickarddahlstrom@Rickards-MBP.lan>
2016-07-20 20:23:50 +0200

    added article new and edit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+    @hello = "hello"
+  end
+
+
+  def create
+    @article = Article.new( article_params )
+    @article.save
+
+    if( @article.save)
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path( @article )
+    else
+      render :new
+    end
+
+    # render plain: @article.errors.full_messages.inspect + Article.last.inspect
+  end
+
+  def edit
+    @article = Article.find( params[:id] )
+  end
+
+  def update
+    @article = Article.find( params[:id] )
+    if @article.update( article_params )
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path( @article )
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @article = Article.find( params[:id] )
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+end
+
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ArticleControllerTest < ActionController::TestCase
+class ArticlesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
