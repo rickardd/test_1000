f0c168630995d0661b0a8b3c3d953f928658e434
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-07-23 08:10:02 +0200

    added users listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -15,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def edit
