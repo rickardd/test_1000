1805fcf6959f0e5b49908e284a202074085ab3bc
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-07-31 10:07:21 +0200

    fix tests and added categories to drop down menu

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create( username: "john", email: "john@example.com", password: "Password", admin: true)
+  end
+
+
   test "get new category form and create category" do
+    sign_in_as( @user, "Password")
     get new_category_path
     assert_template "categories/new"
     assert_difference "Category.count", 1 do
@@ -14,6 +20,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission should result in error message" do
+    sign_in_as( @user, "Password")
     get new_category_path
     assert_template "categories/new"
     assert_no_difference "Category.count" do
